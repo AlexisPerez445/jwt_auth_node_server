18a7ffb78b23ccf59bc608124a348e246fe18704
AlexisPerez445 <traceur35@gmail.com>
2022-12-20 12:49:56 +0100

    mail_config

--- src/mailer/MailerConfig.js
+++ src/mailer/MailerConfig.js
@@ -1,12 +1,17 @@
  const nodemailer = require('nodemailer');
 
+ /*
+ MAILTRAP
+ perezalexis1994@gmail.com
+ Daw2022@
+ */
 const createTrans = () => {
   const transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "2473fed7395f5a",
-      pass: "f2981249b58055"
+      user: "d106616fdcf510",
+      pass: "a7a94325b53614"
     }
   });
   return transport
@@ -16,9 +21,9 @@ const createTrans = () => {
 const sendEmail =  async (nombre, email, token) => {
   const transporter = createTrans();
   const info =await transporter.sendMail({
-    from: '"Fred Foo 👻" <traceur35@gmail.com>',
+    from: '"Fred Foo 👻" <alex_daw@email.com>',
     to: `${email}`, 
-    subject: "CREAR NUEVA PASSWORD", 
+    subject: "RESET PASSWORD", 
     html: `<b>Hola, ${nombre}, <br>
     Puedes crear una nueva contraseña en el siguiente enlace: <br>
     http://localhost:4200/reset-password/${token}
